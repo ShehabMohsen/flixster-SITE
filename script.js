b23178e0d1d3a64bfdee85b6bdf281917d4d31a2
ShehabMohsen <97417536+ShehabMohsen@users.noreply.github.com>
2022-06-10 01:05:01 -0400

    core features working

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ const CURRENT_BTN = document.querySelector("#current-btn");
 const MORE_BTN = document.querySelector("#more-btn")
 
 
+
+
+
 // API key and URLs
 const API_KEY = "e317087c698363ab6080989433dc3835";
 const URL =  `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`;
@@ -36,11 +39,15 @@ async function getMoreMovies(url){
     let dataArray = responseData.results
     dataArray.forEach(element => {    
         if (element.backdrop_path !=null){
-            moviesAREA.innerHTML += `
-            <h3> ${element.original_title} </h3>
-            <img src = "${IMG_PREFIX + element.poster_path}" alt = "movie image" />
-            <p> ${element.vote_average} </p>
-            `
+            moviesAREA.innerHTML += 
+            `<div class = "movie-poster">
+                <img src = "${IMG_PREFIX + element.poster_path}" class = "movie-image" alt = "movie image"/>
+                <div class = "movie-info">
+                    <h3 class = "movie-title"> ${element.original_title} </h3>
+                    <p class = "movie-rating"> ${element.vote_average}/10 </p>
+                    <button class="btn" id="learn-more"> learn more </button>  
+                </div>
+            </div>`
         } 
     });
 }
@@ -54,11 +61,15 @@ function displayMovies(responseData){
     moviesAREA.innerHTML = ``;
     dataArray.forEach(element => {    
         if (element.backdrop_path !=null){
-            moviesAREA.innerHTML += `
-            <div class = "movie-poster">
-            <h3> ${element.original_title} </h3>
-            <img src = "${IMG_PREFIX + element.poster_path}" alt = "movie image" />
-            <p> ${element.vote_average} </p>
+            moviesAREA.innerHTML += 
+            `<div class = "movie-poster">
+                <img src = "${IMG_PREFIX + element.poster_path}" class = "movie-image" alt = "movie image"/>
+                <div class = "movie-info">
+                    <h3 class = "movie-title"> ${element.original_title} </h3>
+                    <p class = "movie-rating"> ${element.vote_average}/10 </p>
+
+                    <button id="learn-more"> learn more modal </button>
+                </div>
             </div>
             `
         } 
@@ -73,22 +84,22 @@ BTN.addEventListener("click", (evt)=>{
     getMovies(url);
 })
 
-
 CURRENT_BTN.addEventListener("click", (evt)=>{
     evt.preventDefault();
     SUBMISSION.value= ''
     getCurrentMovies();
 })
 
+
 MORE_BTN.addEventListener("click", (evt)=>{
     page += 1
     if (SUBMISSION.value == '') {
-    let url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=${page}`;
-    getMoreMovies(url);
+        let url = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=${page}`;
+        getMoreMovies(url);
     }
     else {
-    let url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${SUBMISSION.value}&language=en-US&page=${page}&include_adult=false`;
-    getMoreMovies(url);
+        let url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${SUBMISSION.value}&language=en-US&page=${page}&include_adult=false`;
+        getMoreMovies(url);
     }
 })
 
