40c6d6dbbc48e4b76e363040a6f9e31134470029
ShehabMohsen <97417536+ShehabMohsen@users.noreply.github.com>
2022-06-12 17:13:19 -0400

    added some css for modal

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 // movies currently playing URL = https://api.themoviedb.org/3/movie/now_playing?api_key=<<api_key>>&language=en-US&page=1
 //search URL = https://api.themoviedb.org/3/search/movie?api_key=<<api_key>>&query=${search_value}q&language=en-US&page=1&include_adult=false
 // Query selector stuff:
@@ -9,16 +7,16 @@ const BTN = document.querySelector("#submit-btn");
 const CURRENT_BTN = document.querySelector("#close-search-btn");
 const MORE_BTN = document.querySelector("#load-more-movies-btn")
 
-
-
-
-
 // API key and URLs
 const API_KEY = "e317087c698363ab6080989433dc3835";
 const URL =  `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1`;
 const IMG_PREFIX = "https://image.tmdb.org/t/p/w300/"
 
 var page = 1;
+
+
+
+
 
 // fetching data and displaying movies:
 
@@ -38,7 +36,8 @@ async function getMoreMovies(url){
 
     let dataArray = responseData.results
     dataArray.forEach(element => {    
-        if (element.backdrop_path !=null){
+        if (element.poster_path !=null){
+            console.log(element.original_title, element.id);
             moviesAREA.innerHTML += 
             `<div class = "movie-card">
                 <img src = "${IMG_PREFIX + element.poster_path}" class = "movie-image" alt = "movie image"/>
@@ -47,6 +46,7 @@ async function getMoreMovies(url){
                     <p class = "movie-rating"> ${element.vote_average}/10 </p>
                     <button class="btn" id="learn-more"> learn more </button>  
                 </div>
+                
             </div>`
         } 
     });
@@ -56,25 +56,70 @@ function getCurrentMovies(){
     getMovies(URL);
 }
 
-function displayMovies(responseData){
+async function displayMovies(responseData){
     let dataArray = responseData.results
     moviesAREA.innerHTML = ``;
-    dataArray.forEach(element => {    
-        if (element.backdrop_path !=null){
-            moviesAREA.innerHTML += 
-            `<div class = "movie-card">
-                <img src = "${IMG_PREFIX + element.poster_path}" class = "movie-poster" alt = "movie image"/>
-                <div class = "movie-info">
-                    <h3 class = "movie-title"> ${element.original_title} </h3>
-                    <p class = "movie-votes"> ${element.vote_average}/10 </p>
 
-                    <button id="learn-more"> learn more modal </button>
+    for (let i = 0; i < dataArray.length; i++){
+        if (dataArray[i].backdrop_path !=null){
+            // getting extra data such as overview, release date, etc
+            let moreData = await fetch(`https://api.themoviedb.org/3/movie/${dataArray[i].id}?api_key=${API_KEY}&language=en-US`)
+            let MoreDataObject= await moreData.json();
+            detailsObj = getDetailsObj(MoreDataObject);
+            // console.log(dataArray[i].original_title, detailsObj)
+            // console.log(detailsObj.genres);
+            moviesAREA.innerHTML += 
+            `
+            <div class = "movie-card">
+                <img src = "${IMG_PREFIX + dataArray[i].poster_path}" class = "movie-poster" alt = "movie image"/>
+                <div class = "movie-info">
+                    <h3 class = "movie-title"> ${dataArray[i].original_title} </h3>
+                    <p class = "movie-votes"> ${dataArray[i].vote_average}/10 </p>
+                    
+                    <!--modal start-->
+
+                    <button type = "button" class = "open-modal" data-open = "modal1" id="learn-more">Open Modal</button>
+                    <div class = "modal" id = "modal1">
+                        <div class = "modal-dialog">
+                            <p> Release date: ${detailsObj.release_date} <p>
+                            <p> Genres: ${getGenres(detailsObj.genres)} <p> 
+                            <p> Overview: ${detailsObj.overview} <p>
+                        </div>
+                    </div>
+
+                    <!--modal end-->
+
+
                 </div>
             </div>
             `
         } 
-    });
+    }
+
+    function getGenres(array){
+        let sentence = "";
+        
+        for (let i = 0; i < array.length; i++){
+            sentence += array[i].name 
+            if (i <array.length - 1) sentence += ", ";
+        }
+        console.log("sentence", sentence)
+        return sentence
+    }
 }
+
+// get more details regarding a movie
+function getDetailsObj(responseData){
+    // console.log(responseData);
+    detailsObj = {
+        genres: responseData.genres,
+        overview: responseData.overview,
+        release_date: responseData.release_date,
+        backdrop_path: responseData.backdrop_path,
+    };
+    return detailsObj;
+}
+
 
 // event listeners
 BTN.addEventListener("click", (evt)=>{
@@ -103,8 +148,29 @@ MORE_BTN.addEventListener("click", (evt)=>{
     }
 })
 
+// Scrolling Back To TOp
+//Get the button:
+const SCROLL_BTN = document.getElementById("scroll_up");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+        SCROLL_BTN.style.display = "block";
+    }
+    else SCROLL_BTN.style.display = "none";
+}
+SCROLL_BTN.addEventListener("click", topFunction)
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 
+// as soon as the window loads:
 window.onload = function(){
     getCurrentMovies()
 }
